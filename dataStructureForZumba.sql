9a7faed14e35c4270b04fbda8ab4dced2746ecb7
Ahna Lee <52556893+AhnaLee@users.noreply.github.com>
2021-08-31 09:46:45 +1200

    Add files via upload
    SQL data structure of Zumba Club

--- dataStructureForZumba.sql
+++ dataStructureForZumba.sql
@@ -36,3 +36,44 @@ CREATE TABLE Admins(
 	Ad_add VARCHAR2(100) NOT NULL
 	
 	);
+	
+/* Insert data in to Member table*/
+INSERT INTO Member (Mem_Id, Mem_FName, Mem_LName, Mem_Phone, Mem_Email, Mem_DOB, Mem_ConType)
+VALUES (0001, 'Ahna' ,'Lee', 0213334444, 'student@wintec.ac.nz', 01/01/1987,'concession');
+INSERT INTO Member (Mem_Id, Mem_FName, Mem_LName, Mem_Phone, Mem_Email, Mem_DOB, Mem_ConType)
+VALUES (0002, 'Sunjoo' ,'Yeom', 02122223333, 'student2@wintec.ac.nz', 02/01/1977,'non-concession');
+INSERT INTO Member (Mem_Id, Mem_FName, Mem_LName, Mem_Phone, Mem_Email, Mem_DOB, Mem_ConType)
+VALUES (0003, 'Noah' ,'Pheoung', 0275556666, 'student3@wintec.ac.nz', 03/03/2002,'concession');
+INSERT INTO Member (Mem_Id, Mem_FName, Mem_LName, Mem_Phone, Mem_Email, Mem_DOB, Mem_ConType)
+VALUES (0004, 'Felix' ,'Bangana', 02159998888, 'student4@wintec.ac.nz', 05/15/2000,'concession');
+INSERT INTO Member (Mem_Id, Mem_FName, Mem_LName, Mem_Phone, Mem_Email, Mem_DOB, Mem_ConType)
+VALUES (0005, 'Daniel' ,'Johnson', 0227778989, 'email@gmail.com', 05/30/1994,'non-concession');
+/* Insert data in to Payment Table*/
+INSERT INTO Payment (Payment_Num, Payment_Date, Payment_Amt, Mem_Id)
+VALUES (000001, '08/07/2021' ,250.00, 0001);
+INSERT INTO Payment (Payment_Num, Payment_Date, Payment_Amt, Mem_Id)
+VALUES (000002, '10/07/2021' ,10.00, 0005);
+INSERT INTO Payment (Payment_Num, Payment_Date, Payment_Amt, Mem_Id)
+VALUES (000003, '15/07/2021' ,10.00, 0002);
+INSERT INTO Payment (Payment_Num, Payment_Date, Payment_Amt, Mem_Id)
+VALUES (000004, '20/07/2021' ,150.00, 0003);
+INSERT INTO Payment (Payment_Num, Payment_Date, Payment_Amt, Mem_Id)
+VALUES (000005, '20/07/2021' ,10.00, 0002);
+INSERT INTO Payment (Payment_Num, Payment_Date, Payment_Amt, Mem_Id)
+VALUES (000006, '21/07/2021' ,250.00, 0004);
+/* Insert data in to Classes Table*/
+INSERT INTO Classes (Class_Id, Class_Day, Class_Time, Mem_Id, Admin_Id)
+VALUES ('Beginner', 'Monday' ,0700, 0001,1000);
+INSERT INTO Classes (Class_Id, Class_Day, Class_Time, Mem_Id, Admin_Id)
+VALUES ('Beginner', 'Tuesday' ,1200, 0002,1000);
+INSERT INTO Classes (Class_Id, Class_Day, Class_Time, Mem_Id, Admin_Id)
+VALUES ('Advance', 'Monday' ,1800, 0005,1000);
+INSERT INTO Classes (Class_Id, Class_Day, Class_Time, Mem_Id, Admin_Id)
+VALUES ('Beginner2', 'Friday' ,1800, 0002,1000);
+INSERT INTO Classes (Class_Id, Class_Day, Class_Time, Mem_Id, Admin_Id)
+VALUES ('Beginner', 'Monday' ,0700, 0003,1000);
+INSERT INTO Classes (Class_Id, Class_Day, Class_Time, Mem_Id, Admin_Id)
+VALUES ('Beginner1', 'Saturday' ,1100, 0004,1000);\
+/*Insert data in to Admin table*/
+INSERT INTO Admins (Admin_Id, Ad_Name, Ad_Phone, Ad_Email, Ad_add)
+VALUES (1000, 'Admin-Name' ,0211234567, 'admin@gmail.com', 'Wintec Hamilton Hall');
